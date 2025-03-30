88f78af8fba0729e4c4a3cbb72ac310a6bc8a387
Ankit Suyal <waitonehour@gmail.com>
2025-03-30 20:25:00 +0530

    Added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { AiFillAndroid } from "react-icons/ai";
+import Footer from './Components/Footer';
 
 function App() {
 
   return (
     <>
     <h1>hello</h1>
+    <Footer/>
     </>
   )
 }
